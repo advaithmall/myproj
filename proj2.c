16619d9d9bb38d8eef3c9200b2509762bc2ccd7f
advaithmall <advaith.malladi@research.iiit.ac.in>
2022-02-24 14:21:21 +0530

    third-trial

---
+++ proj2.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main()
+{
+	printf("First addition to git\n");
+	ptintf("made a small change\n");
+	printf("made another one\n");
+	return 0;
+	//lets see how this works out
+}
